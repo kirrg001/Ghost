9a05254d862d1e35866209ed2147810babde6e1d
kirrg001 <katharina.irrgang@googlemail.com>
2016-09-13 21:38:16 +0200

    🎨  adapt bootstrap file because of nconf changes
    refs #6982
    - remove load config
    - remove checkDeprecated
    - adapt arguments for scheduling init
    
    [ci skip]

--- core/server/index.js
+++ core/server/index.js
@@ -3,7 +3,6 @@
 
 // Module dependencies
 var express = require('express'),
-    _ = require('lodash'),
     uuid = require('node-uuid'),
     Promise = require('bluebird'),
     i18n = require('./i18n'),
@@ -62,15 +61,8 @@ function init(options) {
     // Initialize Internationalization
     i18n.init();
 
-    // Load our config.js file from the local file system.
-    return config.load(options.config).then(function () {
-        return config.checkDeprecated();
-    }).then(function loadApps() {
-        return readDirectory(config.get('paths').appPath)
-            .then(function (result) {
-                config.set('paths:availableApps', result);
-            });
-    }).then(function loadThemes() {
+    return readDirectory(config.get('paths').appPath).then(function loadThemes(result) {
+        config.set('paths:availableApps', result);
         return api.themes.loadThemes();
     }).then(function () {
         models.init();
@@ -158,7 +150,11 @@ function init(options) {
 
         // scheduling can trigger api requests, that's why we initialize the module after the ghost server creation
         // scheduling module can create x schedulers with different adapters
-        return scheduling.init(_.extend(config.get('scheduling'), {apiUrl: config.get('url') + utils.url.urlFor('api')}));
+        return scheduling.init({
+            active: config.get('scheduling').active,
+            path: config.get('paths').schedulingPath,
+            apiUrl: config.get('url') + utils.url.urlFor('api')
+        });
     }).then(function () {
         return ghostServer;
     });
