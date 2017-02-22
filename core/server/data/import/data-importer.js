524f575139580430a2ec1245b624552219cbe011
Katharina Irrgang <katharina.irrgang@googlemail.com>
2017-02-22 10:27:46 +0100

    🐛  importing duplicate users (#8016)
    no issue
    
    - if you import a JSON database which contains duplicate users e.g. multiple users are using the same email
    - then the whole database transaction get's rolled back and Ghost fails to import the database

--- core/server/data/import/data-importer.js
+++ core/server/data/import/data-importer.js
@@ -61,7 +61,9 @@ DataImporter.prototype.doUserImport = function (t, tableData, owner, users, role
         return Promise.all(userOps).then(function (descriptors) {
             descriptors.forEach(function (d) {
                 if (!d.isFulfilled()) {
-                    errs = errs.concat(d.reason());
+                    if (!d.reason().raw || (d.reason().raw.errno !== 19 && d.reason().raw.errno !== 1062)) {
+                        errs = errs.concat(d.reason());
+                    }
                 } else {
                     imported.push(d.value().toJSON(internal));
                 }
