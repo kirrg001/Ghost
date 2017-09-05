76c9bc3785640ba0dcee2491f2cde53505dd200d
kirrg001 <katharina.irrgang@googlemail.com>
2017-09-05 12:28:55 +0200

    add changes

--- core/server/utils/image-size.js
+++ core/server/utils/image-size.js
@@ -1,5 +1,60 @@
+var sizeOf = require('image-size'),
+    url = require('url'),
+    Promise = require('bluebird'),
+    got = require('got'),
+    utils = require('../utils'),
+    errors = require('../errors'),
+    config = require('../config'),
+    storage = require('../adapters/storage'),
+    _ = require('lodash'),
+    getLocalFileStoragePath = require('../utils/get-local-file-storage-path'),
+    dimensions,
+    imageObject = {},
+    getImageSizeFromUrl,
+    getImageSizeFromFilePath;
+
+/**
+ * @description compares the imagePath with a regex that reflects our local file storage
+ * @param {String} imagePath as URL or filepath
+ * @returns {Array} if match is true or null if not
+ */
+function isLocalImage(imagePath) {
+    imagePath = utils.url.urlFor('image', {image: imagePath}, true);
+
+    return imagePath.match(new RegExp('^' + utils.url.urlJoin(utils.url.urlFor('home', true), utils.url.getSubdir(), '/', utils.url.STATIC_IMAGE_URL_PREFIX)));
+}
+
+/**
+ * @description processes the Buffer result of an image file
+ * @param {Object} buffer
+ * @returns {Object} dimensions
+ */
+function fetchDimensionsFromBuffer(buffer) {
+    try {
+        // Using the Buffer rather than an URL requires to use sizeOf synchronously.
+        // See https://github.com/image-size/image-size#asynchronous
+        dimensions = sizeOf(buffer);
+        // CASE: `.ico` files might have multiple images and therefore multiple sizes.
+        // We return the largest size found (image-size default is the first size found)
+        if (dimensions.images) {
+            dimensions.width = _.maxBy(dimensions.images, function (w) {return w.width;}).width;
+            dimensions.height = _.maxBy(dimensions.images, function (h) {return h.height;}).height;
+        }
+
+        imageObject.width = dimensions.width;
+        imageObject.height = dimensions.height;
+
+        return Promise.resolve(imageObject);
+    } catch (err) {
+        return Promise.reject(new errors.InternalServerError({
+            code: 'IMAGE_SIZE',
+            err: err
+        }));
+    }
+}
+
 // Supported formats of https://github.com/image-size/image-size:
-// BMP, GIF, JPEG, PNG, PSD, TIFF, WebP, SVG
+// BMP, GIF, JPEG, PNG, PSD, TIFF, WebP, SVG, ICO
 // ***
 // Takes the url of the image and an optional timeout
 // getImageSizeFromUrl returns an Object like this
@@ -8,32 +63,29 @@
 //     url: 'http://myblog.com/images/cat.jpg',
 //     width: 50
 // };
-// if the dimensions can be fetched and rejects with error, if not.
+// if the dimensions can be fetched, and rejects with error, if not.
 // ***
-// In case we get a locally stored image or a not complete url (like //www.gravatar.com/andsoon),
-// we add the protocol to the incomplete one and use urlFor() to get the absolute URL.
+// In case we get a locally stored image, which is checked withing the `isLocalImage`
+// function we switch to read the image from the local file storage with `getImageSizeFromFilePath`.
+// In case the image is not stored locally and is missing the protocol (like //www.gravatar.com/andsoon),
+// we add the protocol and use urlFor() to get the absolute URL.
 // If the request fails or image-size is not able to read the file, we reject with error.
-
-var sizeOf = require('image-size'),
-    url = require('url'),
-    Promise = require('bluebird'),
-    got = require('got'),
-    config = require('../config'),
-    utils = require('../utils'),
-    errors = require('../errors'),
-    dimensions;
 
 /**
  * @description read image dimensions from URL
- * @param {String} imagePath
+ * @param {String} imagePath as URL
  * @returns {Promise<Object>} imageObject or error
  */
-module.exports.getImageSizeFromUrl = function getImageSizeFromUrl(imagePath) {
-    var imageObject = {},
-        requestOptions,
+getImageSizeFromUrl = function getImageSizeFromUrl(imagePath) {
+    var requestOptions,
         timeout = config.get('times:getImageSizeTimeoutInMS') || 10000;
 
     imageObject.url = imagePath;
+
+    if (isLocalImage(imagePath)) {
+        // don't make a request for a locally stored image
+        return Promise.resolve(getImageSizeFromFilePath(imagePath));
+    }
 
     // check if we got an url without any protocol
     if (imagePath.indexOf('http') === -1) {
@@ -41,9 +93,6 @@ module.exports.getImageSizeFromUrl = function getImageSizeFromUrl(imagePath) {
         if (imagePath.indexOf('//') === 0) {
             // it's a gravatar url
             imagePath = 'http:' + imagePath;
-        } else {
-            // get absolute url for image
-            imagePath = utils.url.urlFor('image', {image: imagePath}, true);
         }
     }
 
@@ -61,14 +110,8 @@ module.exports.getImageSizeFromUrl = function getImageSizeFromUrl(imagePath) {
         requestOptions
     ).then(function (response) {
         try {
-            // response.body contains the Buffer. Using the Buffer rather than an URL
-            // requires to use sizeOf synchronously. See https://github.com/image-size/image-size#asynchronous
-            dimensions = sizeOf(response.body);
-
-            imageObject.width = dimensions.width;
-            imageObject.height = dimensions.height;
-
-            return Promise.resolve(imageObject);
+            // response.body contains the buffer of the image file
+            return Promise.resolve(fetchDimensionsFromBuffer(response.body));
         } catch (err) {
             return Promise.reject(new errors.InternalServerError({
                 code: 'IMAGE_SIZE',
@@ -101,3 +144,51 @@ module.exports.getImageSizeFromUrl = function getImageSizeFromUrl(imagePath) {
         }
     });
 };
+
+// Supported formats of https://github.com/image-size/image-size:
+// BMP, GIF, JPEG, PNG, PSD, TIFF, WebP, SVG, ICO
+// ***
+// Takes the url or filepath of the image and reads it form the local
+// file storage.
+// getImageSizeFromFilePath returns an Object like this
+// {
+//     height: 50,
+//     url: 'http://myblog.com/images/cat.jpg',
+//     width: 50
+// };
+// if the image is found and dimensions can be fetched, and rejects with error, if not.
+/**
+ * @description read image dimensions from local file storage
+ * @param {String} imagePath
+ * @returns {object} imageObject or error
+ */
+getImageSizeFromFilePath = function getImageSizeFromFilePath(imagePath) {
+    imagePath = utils.url.urlFor('image', {image: imagePath}, true);
+    imageObject.url = imagePath;
+
+    imagePath = getLocalFileStoragePath(imagePath);
+
+    return storage.getStorage()
+        .read({path: imagePath})
+        .then(function readFile(buf) {
+            try {
+                return Promise.resolve(fetchDimensionsFromBuffer(buf));
+            } catch (err) {
+                return Promise.reject(new errors.InternalServerError({
+                    code: 'IMAGE_SIZE',
+                    err: err,
+                    context: imagePath
+                }));
+            }
+        }).catch(function (err) {
+            return Promise.reject(new errors.InternalServerError({
+                message: err.message,
+                code: 'IMAGE_SIZE',
+                err: err,
+                context: imagePath
+            }));
+        });
+};
+
+module.exports.getImageSizeFromUrl = getImageSizeFromUrl;
+module.exports.getImageSizeFromFilePath = getImageSizeFromFilePath;
