5e37af26d4bd16b258a1996651b3ea2ccb6bfe91
kirrg001 <katharina.irrgang@googlemail.com>
2019-01-29 19:00:44 +0100

    Added permissions for actions
    refs #10431
    
    - migration script to add permissions for actions
    - any role can browse actions

--- core/test/unit/data/schema/fixtures/utils_spec.js
+++ core/test/unit/data/schema/fixtures/utils_spec.js
@@ -150,19 +150,19 @@ describe('Migration Fixture Utils', function () {
             fixtureUtils.addFixturesForRelation(fixtures.relations[0]).then(function (result) {
                 should.exist(result);
                 result.should.be.an.Object();
-                result.should.have.property('expected', 59);
-                result.should.have.property('done', 59);
+                result.should.have.property('expected', 64);
+                result.should.have.property('done', 64);
 
                 // Permissions & Roles
                 permsAllStub.calledOnce.should.be.true();
                 rolesAllStub.calledOnce.should.be.true();
-                dataMethodStub.filter.callCount.should.eql(59);
+                dataMethodStub.filter.callCount.should.eql(64);
                 dataMethodStub.find.callCount.should.eql(5);
-                baseUtilAttachStub.callCount.should.eql(59);
+                baseUtilAttachStub.callCount.should.eql(64);
 
-                fromItem.related.callCount.should.eql(59);
-                fromItem.findWhere.callCount.should.eql(59);
-                toItem[0].get.callCount.should.eql(118);
+                fromItem.related.callCount.should.eql(64);
+                fromItem.findWhere.callCount.should.eql(64);
+                toItem[0].get.callCount.should.eql(128);
 
                 done();
             }).catch(done);
